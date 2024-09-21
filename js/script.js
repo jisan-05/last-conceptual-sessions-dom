45ee7e8a8f14a67d5b14df339e968f095b3681d0
Tanvir <zishantanvir05@gmail.com>
2024-09-21 22:08:24 +0600

    added calculation function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Getting all the value
+
+//add event listener for Calculate Button
 const calculateButton = document.getElementById("calculate-button");
 calculateButton.addEventListener("click", function (event) {
     event.preventDefault();
@@ -5,7 +8,6 @@ calculateButton.addEventListener("click", function (event) {
     const software = parseFloat(document.getElementById("software").value);
     const courses = parseFloat(document.getElementById("courses").value);
     const internet = parseFloat(document.getElementById("internet").value);
-
     const totalExpense = software + courses + internet;
     const balance = income - totalExpense;
 
@@ -15,6 +17,29 @@ calculateButton.addEventListener("click", function (event) {
     const addbalance = document.getElementById("newBalance");
     addbalance.innerText = balance.toFixed(2);
 
-    document.getElementById('result-div').classList.remove('hidden')
+    document.getElementById("result-div").classList.remove("hidden");
+});
 
+//add event listener for Saving Button
+document.getElementById("saving-btn").addEventListener("click", function () {
+    const savingValue = parseFloat(
+        document.getElementById("saving-input").value
+    );
+    console.log(savingValue);
+    const income = parseFloat(document.getElementById("income").value);
+    const software = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+
+    const totalExpense = software + courses + internet;
+    const balance = income - totalExpense;
+
+    const savingAmount = (savingValue * balance) / 100;
+    console.log(savingAmount);
+
+    const remainingBalance = balance - savingAmount;
+    document.getElementById("remainingBalance").innerText =
+        remainingBalance.toFixed(2);
+
+    document.getElementById("savingAmount").innerText = savingAmount.toFixed(2);
 });
